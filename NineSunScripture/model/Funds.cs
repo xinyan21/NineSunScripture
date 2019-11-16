e365e138d15b9b450fc857a1ccbdc1969124539d
N羿 <xinyan621@outlook.com>
2019-11-16 22:06:28 +0800

    初始化

---
+++ NineSunScripture/model/Funds.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineSunScripture.model
+{
+    /*
+     资金
+    */
+    class Funds
+    {
+        //总资产
+        public double totalAsset;
+        //资金余额
+        public double remainingFund;
+        //冻结金额
+        public double frozenAmt;
+        //可用金额
+        public double availableAmt;
+    }
+}
---
+++ NineSunScripture/model/Quotes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineSunScripture.model
+{
+    /*
+     行情实体类
+         */
+    class Quotes
+    {
+        public String code;
+        public String name;
+        //昨收
+        public float preClose;
+        //涨停价
+        public float highLimit;
+        //跌停价
+        public float lowLimit;
+        public float high;
+        public float low;
+        //成交金额
+        public float money;
+        //成交量
+        public float volume;
+        public float open;
+        public float sell1;
+        public float sell2;
+        public float sell3;
+        public float sell4;
+        public float sell5;
+        public float buy1;
+        public float buy2;
+        public float buy3;
+        public float buy4;
+        public float buy5;
+        public float sell1Vol;
+        public float sell2Vol;
+        public float sell3Vol;
+        public float sell4Vol;
+        public float sell5Vol;
+        public float buy1Vol;
+        public float buy2Vol;
+        public float buy3Vol;
+        public float buy4Vol;
+        public float buy5Vol;
+
+    }
+}
